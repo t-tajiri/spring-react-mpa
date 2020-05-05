afa23512ff9f0edce9f20547032297ed183b00d1
t-tajiri <swallow.ava@gmail.com>
2020-05-05 20:01:57 +0900

    first commit

---
+++ src/main/java/ttajiri/example/SpringReactMpaApplication.java
@@ -0,0 +1,13 @@
+package ttajiri.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactMpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactMpaApplication.class, args);
+    }
+
+}
