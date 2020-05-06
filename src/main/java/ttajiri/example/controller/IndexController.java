a4b2660714d6518bdfe78468ae9ad70050a4e94d
t-tajiri <swallow.ava@gmail.com>
2020-05-06 21:13:30 +0900

    feat: add index controller and html

---
+++ src/main/java/ttajiri/example/controller/IndexController.java
@@ -0,0 +1,12 @@
+package ttajiri.example.controller;
+
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class IndexController {
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+}
